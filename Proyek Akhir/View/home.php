8ebdf7d02e7b7e21042fe6518c0e049f4e5ad262
liondy <44316758+liondy@users.noreply.github.com>
2019-04-29 15:56:33 +0700

    melanjutkan homepage

--- Proyek Akhir/View/home.php
+++ Proyek Akhir/View/home.php
@@ -1,26 +1,32 @@
+<div class="content">
+    <h1>Silahkan pilih keluhan</h1>
+    <div class="pilih">
+        <select name="dropdownPenyakit" id="pilihPenyakit">
+            <option value="">- Pilih Keluhan -</option>
+            <?php
+                foreach($res as $key => $value){
+                    echo "<option>";
+                    echo "$value[0]";
+                    echo "</option>";
+                }
+            ?>
+        </select>
+    </div>
+    <div class="sumit">
+        <form action="" method="get">
+            <input type="submit" id="btnMulai" value="Mulai">
+        </form>
+    </div>
+</div>
 <div class="nav">
-    <p id="halonama">Halo, <?=$nama?>!</p>
-    <div class="menu">
-        <a href="home">Home</a>
+    <div class="menuKiri">
+        <p id="halonama">Halo, <?=$nama?>!</p>
+    </div>
+    <div class="menuKanan">
+        <a href="homepage">Home</a>
         <span id="pembatas">|</span>
         <a href="">Lihat Riwayat Penyakit</a>
         <span id="pembatas">|</span>
         <a href="index.php">Logout</a>
     </div>
 </div>
-
-
-<h1>Silahkan pilih keluhan</h1>
-<select name="dropdownPenyakit" id="pilihPenyakit">
-    <option value="">- Pilih Keluhan -</option>
-    <?php
-        foreach($res as $key => $value){
-            echo "<option>";
-            echo "$value[0]";
-            echo "</option>";
-        }
-    ?>
-</select>
-<form action="" method="get">
-    <input type="submit" id="btnMulai" value="Mulai">
-</form>
