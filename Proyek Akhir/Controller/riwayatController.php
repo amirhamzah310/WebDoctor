31aac5bc3eae84512275b5876dbcfc5ef518dbf2
Indra Permana Sugianto <49990236+inpers273@users.noreply.github.com>
2019-04-30 17:17:26 +0700

    Mengedit query di adminController dan riwayatController

--- Proyek Akhir/Controller/riwayatController.php
+++ Proyek Akhir/Controller/riwayatController.php
@@ -8,23 +8,8 @@
             $this->db = new mysqlDB("localhost","root","","webdoctor");
         }
 
-        public function start(){
-            //query mungkin bisa lebih sederhana
-            $query = "SELECT 
-                        diagnosa.waktu AS Tanggal, 
-                        penyakit.namaPenyakit AS Penyakit, 
-                        kategori.namaKategori AS Kategori, 
-                        gejala.namaGejala AS Gejala 
-                    FROM 
-                        diagnosa 
-                    INNER JOIN 
-                        penyakit ON diagnosa.kodePenyakit = penyakit.kodePenyakit 
-                    INNER JOIN 
-                        kategori ON penyakit.idKategori = kategori.idKategori 
-                    INNER JOIN 
-                        hubungan ON penyakit.kodePenyakit = hubungan.kodePenyakit 
-                    INNER JOIN 
-                        gejala ON hubungan.kodeGejala = gejala.kodeGejala";
+        public function start(){            
+            $query = "SELECT diagnosa.waktu AS Tanggal, penyakit.namaPenyakit AS Penyakit, kategori.namaKategori AS Kategori, gejala.namaGejala AS Gejala FROM diagnosa INNER JOIN penyakit ON diagnosa.kodePenyakit = penyakit.kodePenyakit INNER JOIN kategori ON penyakit.idKategori = kategori.idKategori INNER JOIN hubungan ON penyakit.kodePenyakit = hubungan.kodePenyakit INNER JOIN gejala ON hubungan.kodeGejala = gejala.kodeGejala";
             $res = $this->db->executeSelectQuery($query);
             return View::createView('riwayat.php', []);
         }
