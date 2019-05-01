4b3166a4dd38c9598ea554177808110130d564c9
liondy <44316758+liondy@users.noreply.github.com>
2019-05-01 20:01:41 +0700

    memberi styling pada halaman profile

--- Proyek Akhir/View/profile.php
+++ Proyek Akhir/View/profile.php
@@ -1,22 +1,24 @@
 <div class="header">
     <h1 class="judul">PROFIL <?=strtoupper($nama)?></h1>
 </div>
+<div class="profile">
 <div class="foto">
-    <form action="upload" method="post" enctype="multipart/form-data">
+    <form action="upload" method="post" id="lblFoto" enctype="multipart/form-data">
         <label for="profileImage">Foto Anda</label><br>
         <?php
             echo "<img src='View/Style/uploads/".$res[0][8]."' id='displayPicture' onclick='trigger()'>";
         ?>
         File yang diupload maksimum berukuran 30 x 30
         <input type="file" name="upfile" id="upfile" onchange="display(this)"><br>
-        <input type="submit" value="Simpan">
+        <input type="submit" value="Simpan" id="simpan">
     </form>
 </div>
-<div class="konten">
+<div class="profil">
     Username : <?=$res[0][0]?><br>
     Tanggal Lahir : <?=$res[0][2]?><br>
     Kota : <?=$res[0][3]?><br>
     Alamat : <?=$res[0][4]?><br>
     Email : <?=$res[0][5]?><br>
     Tanggal Bergabung : <?=$res[0][9]?>
+</div>
 </div>
