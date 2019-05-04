cdf92ad569859febc9042b610077e3cd2bd2a858
liondy <44316758+liondy@users.noreply.github.com>
2019-05-04 11:11:21 +0700

    pdf fixed (masih di 1 halaman)

--- Proyek Akhir/cetak.php
+++ Proyek Akhir/cetak.php
@@ -1,7 +1,7 @@
 <?php
 
     require_once __DIR__ . '/vendor/autoload.php';
-    include "main.php";
+    include 'main.php';
 
     $mpdf = new \Mpdf\Mpdf();
     $html = '
@@ -9,8 +9,10 @@
     <head>
         <link rel="stylesheet" href="View/Style/style.css">        
     </head>
-    <body> 
-    <h1 class="judul">Daftar Penyakit Gejala Kategori</h1>
+    <body>
+    <div class="header">
+        <h1 class="judul">Daftar Penyakit Gejala Kategori</h1>
+    </div>
     <table>
     <tr>
         <th>No</th>
@@ -22,34 +24,34 @@
         <th>Nama Kategori</th>
     </tr>';
     foreach ($res as $key => $value) {
-        $html.= "<tr>";
-        $html.= "<td>";
+        $html.= '<tr>';
+        $html.= '<td>';
         $html.= ($key+1);
-        $html.= "</td>";
-        $html.= "<td>";
-        $html.= "$value[0]";
-        $html.= "</td>";
-        $html.= "<td>";
-        $html.= "$value[1]";
-        $html.= "</td>";
-        $html.= "<td>";
-        $html.= "$value[2]";
-        $html.= "</td>";
-        $html.= "<td>";
-        $html.= "$value[3]";
-        $html.= "</td>";
-        $html.= "<td>";
-        $html.= "$value[4]";
-        $html.= "</td>";
-        $html.= "<td>";
-        $html.= "$value[5]";
-        $html.= "</td>";
-        $html.= "</tr>";
+        $html.= '</td>';
+        $html.= '<td>';
+        $html.= $value[0];
+        $html.= '</td>';
+        $html.= '<td>';
+        $html.= $value[1];
+        $html.= '</td>';
+        $html.= '<td>';
+        $html.= $value[2];
+        $html.= '</td>';
+        $html.= '<td>';
+        $html.= $value[3];
+        $html.= '</td>';
+        $html.= '<td>';
+        $html.= $value[4];
+        $html.= '</td>';
+        $html.= '<td>';
+        $html.= $value[5];
+        $html.= '</td>';
+        $html.= '</tr>';
     }
-    $html.="</table>";
-    $html.="</body>";
-    $html.="</html>";
+    $html.='</table>';
+    $html.='</body>';
+    $html.='</html>';
     echo $html;
-    // $mpdf->WriteHTML($html);
-    // $mpdf->Output();
+    $mpdf->WriteHTML($html);
+    $mpdf->Output();
 ?>
--- Proyek Akhir/main.php
+++ Proyek Akhir/main.php
@@ -1,6 +1,6 @@
 <?php
     require_once "Controller/mysqlDB.php";
     $db = new mysqlDB("localhost","root","","webdoctor");
-    $query = "SELECT `kodePenyakit`, `namaPenyakit`, `kodeGejala`, `namaGejala`, `idKategori`, `namaKategori` FROM `penyakit` INNER JOIN `hubungan` ON `penyakit`.`kodePenyakit` = `hubungan.kodePenyakit` INNER JOIN `gejala` ON `hubungan`.`kodeGejala` = `gejala`.`kodeGejala` INNER JOIN `kategori` ON `penyakit`.`idKategori` = `kategori`.`idKategori`";
+    $query = "SELECT `penyakit`.`kodePenyakit`, `namaPenyakit`, `gejala`.`kodeGejala`, `namaGejala`, `kategori`.`idKategori`, `namaKategori` FROM `penyakit` INNER JOIN `hubungan` ON `penyakit`.`kodePenyakit` = `hubungan`.`kodePenyakit` INNER JOIN `gejala` ON `hubungan`.`kodeGejala` = `gejala`.`kodeGejala` INNER JOIN `kategori` ON `penyakit`.`idKategori` = `kategori`.`idKategori`";
     $res = $db->executeSelectQuery($query);
 ?>
